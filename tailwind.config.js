23a0d9a4bba7321b4e8038ba786309aae95197f7
corbin.boonzaier <corbin.boonzaier@googlemail.com>
2021-02-10 12:39:08 +0200

    layout done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: (theme) => ({
+        'gavel': "url('/assets/img/backgroundcontacts.jpg')",
+      }),
+    },
   },
   variants: {
     extend: {},
